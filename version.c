5a135671a0520e587f8965c965fcdcaebf2d5956
lifeng <lifeng@sino-data.com>
2016-01-04 11:01:49 +0800

    chang version.c context to 2.0.1

---
+++ version.c
@@ -0,0 +1,14 @@
+/*
+ * version.c
+ * Automatically generated
+ */
+
+#include "libss7.h"
+
+static const char ss7_version[] = "2.0.1";
+
+const char *ss7_get_version(void)
+{
+	return ss7_version;
+}
+
